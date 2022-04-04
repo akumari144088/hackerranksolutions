e68731425a3ac31995d040581a7a7866fac644e4
akumari144088 <40607571+akumari144088@users.noreply.github.com>
2022-04-04 10:32:49 +0530

    Create averageofsets.py

---
+++ averageofsets.py
@@ -0,0 +1,33 @@
+problem statement:-
+  Ms. Gabriel Williams is a botany professor at District College. One day, she asked her student Mickey to compute the average of all the plants with distinct heights in her greenhouse.
+
+Formula used:
+
+Function Description
+
+Complete the average function in the editor below.
+
+average has the following parameters:
+
+int arr: an array of integers
+Returns
+
+float: the resulting float value rounded to 3 places after the decimal
+
+
+
+solution:-
+  
+def average(array):
+    # your code goes here
+      uniquearray= set(array)
+      return sum(uniquearray)/len(uniquearray)
+     
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+    result = average(arr)
+    print(result)
+    
+    
